636a7d47be5cf68c845b1badf75f7d4c2cb1a4b9
e-landeros <landerosedgard@gmail.com>
2018-05-24 15:15:36 -0700

    update hosts file overwrites
    truncated method and file.seek to position pointer for overwrites

--- blocker_app.py
+++ blocker_app.py
@@ -11,9 +11,9 @@ redirect="127.0.0.1"
 website_list=['www.facebook.com','facebook.com','www.reddit.com','reddit.com']
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day,8)<dt.now()<dt(dt.now().year,dt.now().month,dt.now().day,16):
+    if dt(dt.now().year, dt.now().month, dt.now().day,18)<dt.now()<dt(dt.now().year,dt.now().month,dt.now().day,19):
         print("Working Hours...")
-        #read and write to hosts file
+        #read and write to hosts file w create new empty file so use r+ to read and write
         with open(hosts_temp, 'r+') as file:
             content=file.read()
             for website in website_list:
@@ -22,6 +22,19 @@ while True:
                 else:
                     file.write(redirect+" "+website+"\n")
     else:
+        #cannot detlete added lines in hosts directly
+        #check for website_list == readlines list
+        with open(hosts_temp,'r+') as file:
+            #reads file content and stores each line in a list
+            content=file.readlines()
+            #to move the pointer from end of readline to before the first char
+            file.seek(0)
+            for line in content:
+                #loop each website in website_list
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            #avoid repeating text in the file every loop
+            file.truncate()
         print("Fun Hours...")
-    #run every 5 sec not every millisec
+    #run every 5 sec not continously
     time.sleep(5)
