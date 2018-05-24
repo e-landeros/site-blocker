36a2274e92a759b4b0bfa83dc00c72d229ac6a7b
e-landeros <landerosedgard@gmail.com>
2018-05-24 14:00:26 -0700

    inti commit
    python app to run local process and block ip addresses within specified
    time

---
+++ blocker_app.py
@@ -0,0 +1,16 @@
+import time
+from datetime import datetime as dt
+
+"""
+Modify hosts file on computer to block access to specific ip addresses within a
+time frame eg.8-5pm
+"""
+hosts_path =r"/etc/hosts"
+redirect="127.0.0.1"
+website_list=['www.facebook.com','facebook.com','www.reddit.com','reddit.com']
+
+while True:
+    if
+    print(1)
+    #print every 5 sec not every millisec
+    time.sleep(5)
